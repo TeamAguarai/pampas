3a68ce0795fdc07fb1e9e71b8876350df9de7da9
TeamAguarai <teamaguarai@gmail.com>
2024-12-27 16:00:14 -0300

    Eliminado comentario inutil

--- core/Writer.cpp
+++ core/Writer.cpp
@@ -40,28 +40,3 @@ void Writer::close()
         is_open = false;
     }
 }
-
-
-// int main() {
-//     // Crear una instancia de la clase CSVWriter
-//     Writter csv("datos.csv", "Ancho de Pulso (ms),Velocidad (m/s)");
-
-//     // Verificar si el archivo se abrió correctamente
-//     if (!csv.is_file_open()) {
-//         return 1; // Salir si el archivo no se abrió
-//     }
-
-//     // Simular lecturas de datos y escribir en el archivo
-//     for (int i = 0; i < 10000; ++i) {
-//         int pulse_width = 1000 + i;           // Ejemplo de ancho de pulso
-//         double speed = pulse_width * 0.001;   // Ejemplo de velocidad en m/s
-
-//         csv.write_row(pulse_width, speed);    // Escribir datos en el archivo
-//     }
-
-//     // Cerrar el archivo explícitamente (opcional, el destructor también lo hace)
-//     csv.close();
-
-//     std::cout << "Datos guardados exitosamente en datos.csv\n";
-//     return 0;
-// }
