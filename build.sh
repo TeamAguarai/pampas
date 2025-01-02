eb57e6f5d21d1c24bfa9914d8de4e73d26c6d2af
TeamAguarai <teamaguarai@gmail.com>
2025-01-02 19:46:56 -0300

    archivo de instalación rápida

---
+++ build.sh
@@ -0,0 +1,13 @@
+if command -v gpio &> /dev/null
+then
+    echo "WiringPi está instalado. (Omitiendo su instalacion)"
+else
+    echo "WiringPi no está instalado. Instalando ahora...."
+    sudo apt-get purge wiringpi
+    hash -r
+    git clone https://github.com/WiringPi/WiringPi.git
+    cd WiringPi
+    ./build
+fi
+
+make
