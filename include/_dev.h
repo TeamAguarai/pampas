3d8d336e436a31cdb7b520916f5a73a8abf7965e
TeamAguarai <teamaguarai@gmail.com>
2025-01-02 23:57:18 -0300

    se añade funcionalidad _dev
    solo para desarrollo interno

---
+++ include/_dev.h
@@ -0,0 +1,10 @@
+/*
+* _basic: funcionalidad para desarrollo interno
+*/
+
+#include <iostream>
+
+namespace control
+{
+void hello();
+}
---
+++ src/_dev.cpp
@@ -0,0 +1,18 @@
+/*
+* _basic: funcionalidad para desarrollo interno
+*/
+
+#ifdef DEV
+    #include "_dev.h"
+#else
+    #include "control.h"
+#endif
+
+namespace control {
+
+void hello() 
+{
+    std::cout << "HELLO FROM CONTROL!" << std::endl;
+}
+
+}
