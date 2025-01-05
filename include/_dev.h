8ee2a7392faefa01c2b6e8c09d6d5134c7ff64da
TeamAguarai <teamaguarai@gmail.com>
2025-01-05 17:54:24 -0300

    Actualizado includes para libreria

--- include/_dev.h
+++ include/_dev.h
@@ -2,7 +2,6 @@
 * _dev: funcionalidad para desarrollo interno
 */
 
-#include <iostream>
 
 #if defined(PAMPAS_DEV) && defined(PAMPAS_LIBRARY)
     #error "No se puede definir PAMPAS_DEV y PAMPAS_LIBRARY al mismo tiempo."
@@ -15,6 +14,8 @@
 #ifdef PAMPAS_LIBRARY
     #include "pampas.h"
 #endif
+
+#include <iostream>
 
 namespace pampas
 {
--- src/_dev.cpp
+++ src/_dev.cpp
@@ -2,11 +2,18 @@
 * _dev: funcionalidad para desarrollo interno
 */
 
-#ifdef DEV
+#if defined(PAMPAS_DEV) && defined(PAMPAS_LIBRARY)
+    #error "No se puede definir PAMPAS_DEV y PAMPAS_LIBRARY al mismo tiempo."
+#endif
+
+#ifdef PAMPAS_DEV
     #include "_dev.h"
-#else
+#endif
+
+#ifdef PAMPAS_LIBRARY
     #include "pampas.h"
 #endif
+
 
 namespace pampas {
 
